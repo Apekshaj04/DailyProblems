9eb83ebe37472a2ad9850dfb16bc15eede0b6ed7
Apekshaj04 <117656894+Apekshaj04@users.noreply.github.com>
2025-02-20 20:33:49 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (99.48%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,39 +1,9 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-
 class Solution {
-    public void recursiveCalls(StringBuilder sb, int index, int n, ArrayList<String> arr) {
-        if (sb.length() == n) { 
-            arr.add(sb.toString());
-            return;
-        }
-        
-        sb.append("0");
-        recursiveCalls(sb, index + 1, n, arr);
-        sb.deleteCharAt(sb.length() - 1);
-
-        sb.append("1");
-        recursiveCalls(sb, index + 1, n, arr);
-        sb.deleteCharAt(sb.length() - 1);
-    }
-
     public String findDifferentBinaryString(String[] nums) {
-        ArrayList<String> arr = new ArrayList<>();
-        int n = nums.length;
-        
-        recursiveCalls(new StringBuilder(), 0, n, arr);
-        
-        HashSet<String> never = new HashSet<>();
-        for (String x : nums) {
-            never.add(x);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0;i<nums.length;i++){
+            sb.append(nums[i].charAt(i)=='0'?'1':'0');
         }
-        
-        for (String y : arr) {
-            if (!never.contains(y)) {
-                return y;
-            }
-        }
-        
-        return "";
+        return sb.toString();
     }
 }

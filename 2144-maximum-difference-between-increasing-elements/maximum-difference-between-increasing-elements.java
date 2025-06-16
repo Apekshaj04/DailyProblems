6a35d5a5cc6540223f27e5a6f6e57d80127f1451
Apeksha Jamjar <117656894+Apekshaj04@users.noreply.github.com>
2025-06-16 12:04:17 +0530

    Time: 1 ms (57.39%) | Memory: 42 MB (73.85%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
@@ -2,11 +2,16 @@ class Solution {
     public int maximumDifference(int[] nums) {
         int max = -1;
         int n = nums.length;
-        for(int i = 0;i<n;i++){
-            for(int j = i+1;j<n;j++){
-                if(nums[j]>nums[i]){
-                    max = Math.max(max,nums[j]-nums[i]);
-                }
+        int[] arr = new int[n];
+        arr[n-1] = nums[n-1];
+        for(int i = n-2;i>=0;i--){
+            arr[i] = Math.max(arr[i+1],nums[i]);
+        }
+
+        for(int i =0;i<n-1;i++){
+            if(arr[i]>nums[i]){
+
+            max = Math.max(max,arr[i]-nums[i]);
             }
         }
         return max;

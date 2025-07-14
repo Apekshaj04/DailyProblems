4011d00f5bb40527daff711f8de5ac17899970ea
Apeksha Jamjar <117656894+Apekshaj04@users.noreply.github.com>
2025-07-14 20:41:00 +0530

    Time: 438 ms (26.98%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,2 +1,2 @@
-/* Write your PL/SQL query statement below */
-SELECT p.firstName AS FirstName,P.lastName  AS LastName,a.city AS City,a.state AS State from PERSON p  LEFT JOIN ADDRESS a ON p.personId = a.personId;
+# Write your MySQL query statement below
+select p.firstName,p.lastName,a.city,a.state from Person p left join Address a on a.personId = p.personId ;
